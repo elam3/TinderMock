e39f94f639d041173ff42e065c91ef21cc33be73
Edison L <elam3@mail.ccsf.edu>
2017-04-06 23:39:00 -0700

    added a ui pan gesture recognizer

--- TinderMock/CardsViewControllerViewController.swift
+++ TinderMock/CardsViewControllerViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class CardsViewControllerViewController: UIViewController {
-
+    
+    @IBOutlet weak var cardImageView: UIImageView!
+    
+    var cardInitialCenter: CGPoint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,7 +25,29 @@ class CardsViewControllerViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func didPanCard(_ sender: Any) {
+        // Common properties to access from Pan Gesture Recognizer
+        let location = (sender as AnyObject).location(in: view)
+        let translation = (sender as AnyObject).translation(in: view)
+        let velocity = (sender as AnyObject).velocity(in: view)
+        
+        print("translation \(translation)")
+        
+        if (sender as AnyObject).state == .began {
+            cardInitialCenter = cardImageView.center
+        } else if (sender as AnyObject).state == .changed {
+            
+        } else if (sender as AnyObject).state == .ended {
+            
+        }
+    }
 
+    @IBAction func onMsgBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func onPrefBtnPressed(_ sender: Any) {
+    }
+    
     /*
     // MARK: - Navigation
 
